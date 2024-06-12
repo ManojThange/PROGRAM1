bb0eed01e804484e8460f0c0fda474c5d3b24b3f
Manoj <manojthange1112@gmail.com>
2024-06-12 11:34:47 +0530

    add all files

---
+++ app.js
@@ -0,0 +1,49 @@
+// let user = prompt("enter the max number");
+
+// const random =Math.floor(Math.random() *user)+1;
+// let guess = prompt("guess the number");
+// while(true){
+//     if(guess == "quit"){
+//         console.log("user quit");
+//         break;
+//     }
+//     if(guess == random){
+//         console.log("you are right ! congrats the random number is ",random);
+//         break;
+//     }else if(guess < random){
+//      guess =prompt("hint : you are guess was to small.please try again");
+//     }else{
+//         guess=prompt("hint :you are guess was to large .please try again")
+//     }
+// }
+
+let car=[
+    {
+        name :"jeep",
+        model :2019,
+        color:"black"
+    },
+    {
+        name :"BMZ",
+        model:2023,
+        color:"purple"
+    },
+    {
+        name:"Porche",
+        model:2043,
+        color:"blue"
+    },
+];
+
+const person=[
+    {
+        name:"vishal",
+        age : 23,
+        city :"dubai"
+    },
+    {
+        name:"rahul",
+        age:21,
+        city:"india"
+    },
+];
